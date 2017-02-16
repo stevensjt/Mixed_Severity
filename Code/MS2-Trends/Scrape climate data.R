ac7a04f2379f263b1f112377f8de5521f9c1a03e
stevensjt <stevensjt@gmail.com>
2017-02-15 23:03:03 -0800

    Updated climate data

--- Code/MS2-Trends/Scrape climate data.R
+++ Code/MS2-Trends/Scrape climate data.R
@@ -3,6 +3,7 @@
 library(ggplot2)
 library(plyr)
 library(rgeos)# For gCentroid
+library(rnoaa)
 source(file="./Analyses/Functions.R")
 
 ####1. Read in data####
@@ -21,3 +22,13 @@ hs_fire=fill_holes(hs_fire=hs_fire)
 plot(hs_fire,axes=T)
 points(gCentroid(hs_fire))
 
+#Possibilities for accessing noaa data:
+#ftp://cran.r-project.org/pub/R/web/packages/rnoaa/rnoaa.pdf
+#https://ropensci.org/tutorials/rnoaa_tutorial.html
+#https://www.ncdc.noaa.gov/cdo-web/token
+
+ncdc_datasets(token ="FTEIRBcwLUCArmZqFHwVomZhjQDOCqMv")
+ncdc_locs(datasetid='GHCND',token ="FTEIRBcwLUCArmZqFHwVomZhjQDOCqMv")
+station_data <- ghcnd_stations()
+View(meteo_distance(station_data, 37.8375105,-122.2542293, radius = 10, limit = 30))
+#Just need to get LL coords from centroid code, extend radius, and get dates of each fire.
