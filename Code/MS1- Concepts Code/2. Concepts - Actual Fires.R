bc2a62a850689ddb03111507a017bbad77200d72
stevensjt <stevensjt@gmail.com>
2018-03-02 11:56:10 -0800

    Updated Actual Fires.R to include example for Postfire GTR

--- Code/MS1- Concepts Code/2. Concepts - Actual Fires.R
+++ Code/MS1- Concepts Code/2. Concepts - Actual Fires.R
@@ -5,12 +5,18 @@ library(cowplot)
 library(tidyverse)
 library(rgeos) #for gBuffer in section 5; v 0.3-26
 library(raster) #for extent() in section 5; v 2.6-7
+#library(profvis) #See MCM talk DRUG 1/13/18, function to rip apart code chunks and see time reqs. 
+#Also profile -> profile selected lines
+#Also library(microbenchmark) and library(rio) for faster imports (but not for shapefiles)
+
 source("./Code/Functions.R")
 
 ####1a. Load data for analysis####
+#tmp<-profvis({
 fire.list <- read_csv("./Data/Derived/all_fires_ForAnalysis.csv")
 fires_long <- read_csv("./Data/Derived/Long_Form/all_fires_Long.csv")
 hs_patches=readOGR("../Large Files/GIS/BurnSev/Current/", layer="hs_patches")
+#})
 
 ####2.Plot Specific fires####
 #Gives a nice comparison of the East and Caribou fires.
@@ -188,39 +194,62 @@ ggdraw()+
 library(ggpolypath)
 
 ##START HERE; swap out Caribou fire for King fire
-fires.to.plot.names=c("2015ROUGH","2008CARIBOU")
+fires.to.plot.names=c("2015ROUGH","2014KING")
 fires.to.plot=fires_long[as.character(fires_long$name)%in%fires.to.plot.names,]
-fires.to.plot$name=factor(fires.to.plot$name,labels=c("Caribou", "Rough"))
+fires.to.plot$name=factor(fires.to.plot$name,labels=c("King", "Rough"))
+full_shapes <- readOGR("../Large Files/GIS/BurnSev/Specific Fires/", layer="KingRoughFullPerimsGTR") #CRS EPSG:3310, NAD83 CA Albers
 
-p.a_0 <- fill_holes(hs_patches[hs_patches$VB_ID==fires.to.plot.names[1],])
+##PLots
+p.a_hs <- fill_holes(hs_patches[hs_patches$VB_ID==fires.to.plot.names[1],])
+p.a_full <- full_shapes[full_shapes$VB_ID==fires.to.plot.names[1],]
 p.a_120 <- gBuffer(
   createSPComment(fill_holes(hs_patches[hs_patches$VB_ID==fires.to.plot.names[1],])), 
   #createSPComment was done in the analysis used to create the long dataset (from Collins et al), 
   #but needs to be re-done here for plotting a gBuffered layer
   width = -120)
+p.b_hs <- fill_holes(hs_patches[hs_patches$VB_ID==fires.to.plot.names[2],])
+p.b_full <- full_shapes[full_shapes$VB_ID==fires.to.plot.names[2],]
+p.b_120 <- gBuffer(
+  createSPComment(fill_holes(hs_patches[hs_patches$VB_ID==fires.to.plot.names[2],])), 
+  width = -120)
 
 p.a=ggplot()+
-  geom_polypath(data=p.a_0,
-               aes(x=(long-min(extent(p.a_0)@xmin))/1000,
-                   y=(lat-min(extent(p.a_0)@ymin))/1000,group=group),
-               col='darkred',fill='darkred')+
+  geom_polygon(data=p.a_full,
+               aes(x=(long-min(extent(p.a_full)@xmin))/1000,
+                   y=(lat-min(extent(p.a_full)@ymin))/1000,group=group),
+               fill="gray",col="gray")+
+  geom_polypath(data=p.a_hs,
+               aes(x=(long-min(extent(p.a_full)@xmin))/1000,
+                   y=(lat-min(extent(p.a_full)@ymin))/1000,group=group),
+               col='darkred',fill='darkred',size = 0.1)+
   geom_polypath(data=p.a_120,
-                aes(x=(long-min(extent(p.a_0)@xmin))/1000,
-                        y=(lat-min(extent(p.a_0)@ymin))/1000,group=group),
-                    col='aquamarine',fill='aquamarine')+
-  xlim(0,50) + ylim(0,30)+ 
+                aes(x=(long-min(extent(p.a_full)@xmin))/1000,
+                        y=(lat-min(extent(p.a_full)@ymin))/1000,group=group),
+                    col='aquamarine',fill='aquamarine', size = 0.1)+
+  xlim(0,48) + ylim(0,42)+ 
   coord_fixed()+
-  labs(title="Rough Fire (2015)",x=" ",y="km") +
+  labs(title="Rough Fire (2015)",x="km",y="km") +
   theme_bw()+
   theme(axis.text=element_text(size=14),
         axis.title=element_text(size=16),
         plot.title=element_text(size=18, hjust=0.5))
 
 p.b=ggplot()+
-  geom_polypath(data=fill_holes(hs_patches[hs_patches$VB_ID==fires.to.plot.names[2],]),
-               aes(x=(long-min(long))/1000,y=(lat-min(lat))/1000,group=group),fill='darkblue')+
-  xlim(0,50) + ylim(0,30)+ coord_fixed()+
-  labs(title="Caribou Fire (2008)",x=" ", y=element_blank()) +
+  geom_polygon(data=p.b_full,
+               aes(x=(long-min(extent(p.b_full)@xmin))/1000,
+                   y=(lat-min(extent(p.b_full)@ymin))/1000,group=group),
+               fill="gray",col="gray")+
+  geom_polypath(data=p.b_hs,
+                aes(x=(long-min(extent(p.b_full)@xmin))/1000,
+                    y=(lat-min(extent(p.b_full)@ymin))/1000,group=group),
+                col='darkblue',fill='darkblue', size = 0.1)+
+  geom_polypath(data=p.b_120,
+                aes(x=(long-min(extent(p.b_full)@xmin))/1000,
+                    y=(lat-min(extent(p.b_full)@ymin))/1000,group=group),
+                col='aquamarine',fill='aquamarine', size = 0.1)+
+  xlim(0,48) + ylim(0,42)+ 
+  coord_fixed()+
+  labs(title="King Fire (2014)",x="km",y="km") +
   theme_bw()+
   theme(axis.text=element_text(size=14),
         axis.title=element_text(size=16),
@@ -230,20 +259,40 @@ p.fits=ggplot(data=fires.to.plot,aes(x=width,y=prop.hs))+
   geom_point(aes(col=name))+
   geom_point(data = fires.to.plot[fires.to.plot$width == 120,], aes(x=width, y = prop.hs),
              col = "aquamarine", size = 5)+
-  scale_color_manual(values=c("darkred","darkblue"),labels=c("Rough Fire","Caribou Fire")) +
+  scale_color_manual(values=c("darkblue","darkred"),labels=c("King Fire","Rough Fire")) +
   geom_path(aes(x=width,y=1/(10^(sdc*width)),group=name,col=name))+
-  annotate("text", x = c(125,245), y = c(0.25,0.35), label = rev(unique(fires.to.plot$sdc.name)),size=3) +
-  labs(y="proportion \n stand-replacing",x="Patch interior buffer distance (m)",title=" ") +
+  annotate("text", x = c(500,750), y = c(0.10,0.35), 
+           label = paste("sdc = ",rev(unique(fires.to.plot$sdc.name)),
+                         "\nln(sdc) = ",round(log(rev(unique(fires.to.plot$sdc.name))),3)),
+           size=2.5) +
+  labs(y="proportion \n stand-replacing",x="Patch interior \nbuffer distance (m)",title=" ") +
+  theme_bw()+
+  theme(axis.text=element_text(size=13),
+        axis.title=element_text(size=16),
+        legend.position = c(0.9,0.7),legend.justification = c(1, 0.5), 
+        legend.title = element_blank())
+
+h=ggplot(fire.list,aes(log(sdc)))+
+  geom_histogram(binwidth=0.1,fill="white",col="black")+
+  geom_vline(xintercept=log(rev(unique(fires.to.plot$sdc.name))),col=c("darkred","darkblue"),size=1.2)+
+  labs(x="ln(SDC)",y="Number of fires",title=" ")+
+  xlim(-7.5,-3)+
   theme_bw()+
   theme(axis.text=element_text(size=14),
-        axis.title=element_text(size=16),
-        legend.position = c(0.9,0.5),legend.justification = c(1, 0.5), legend.title = element_blank())
+        axis.title=element_text(size=15) )
 
+pdf("/Users/Jens/Documents/Berkeley/Post-Doc UCB/Research Projects/Side Projects/Postfire GTR/SDC_fig.pdf")
 ggdraw()+
   draw_plot(p.a, x=0, y=.5, width=.5, height=.5) +
   draw_plot(p.b, x=.5, y=.5, width=.5, height=.5) +
-  draw_plot(p.fits, x=0, y=0, width=1, height=.5) +
-  draw_plot_label(c("A", "B", "C", "km"), c(0, 0.5, 0, 0.40), c(1, 1, 0.5, 0.55), 
-                  size = c(12, 12, 12, 16), fontface=c("bold","bold","bold","plain")) 
+  draw_plot(p.fits, x=0, y=0, width=.5, height=.5) +
+  draw_plot(h, x=.5, y=0, width=.5, height=.5) +
+  draw_plot_label(c("A", "B", "C", "D"), c(0, 0.5, 0, 0.50), c(1, 1, 0.5, 0.5), 
+                  size = 12, fontface="bold") 
 
-dev.copy2pdf(file=paste0("./Figures/Fig4_",Sys.Date(),".pdf"),width=8,height=8)
+dev.off()
+#sum(area(full_shapes)[c(1,2,3,5,7,9,11)])*0.0001 #King Area
+#sum(area(full_shapes)[c(4,6,8,10,12,13,14)])*0.0001 #Rough Area
+#library(stats)
+quantile(log(fire.list$sdc),0.19)
+
